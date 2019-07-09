9b9adc409dec4bf95b070aeca6634e6bb8975f1c
hyunkee <khk2909@daum.net>
2019-07-09 17:03:20 +0900

    19. 07. 09 test/MemberService 생성

---
+++ src/main/java/kr/green/test/service/MemberService.java
@@ -0,0 +1,12 @@
+package kr.green.test.service;
+
+import org.springframework.stereotype.Service;
+
+import kr.green.test.vo.MemberVO;
+
+@Service
+public interface MemberService {
+
+	public boolean getId(MemberVO mVo);	
+
+}
