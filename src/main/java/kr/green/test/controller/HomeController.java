6609ff208d9c7fdcf32f20c4e82c94cfd3246563
hyunkee <khk2909@daum.net>
2019-07-15 17:14:23 +0900

    19. 07. 15 test/ HomeController 수정

--- src/main/java/kr/green/test/controller/HomeController.java
+++ src/main/java/kr/green/test/controller/HomeController.java
@@ -2,6 +2,9 @@ package kr.green.test.controller;
 
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +27,17 @@ public class HomeController {
 	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model, String id) {
+	public String getHome(Model model, String id) {
 		logger.info("페이지 실행");
+		
+		//String email = memberDAO.getEmail(id);
+		//System.out.println(email);
+		//model.addAttribute("email",email);
+		return "home";
+	}
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String postHome(Model model, String id) {
+		logger.info("페이지 실행중");
 		
 		//String email = memberDAO.getEmail(id);
 		//System.out.println(email);
@@ -34,17 +46,32 @@ public class HomeController {
 	}
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginGet() {
-		logger.info("로그인페이지 실행");
+		logger.info("로그인페이지 확인");
 		
 		return "login";		
 	}
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String loginPost(MemberVO mVo) {
-		logger.info("로그인페이지 확인");
+	public String loginPost(Model model, MemberVO mVo) {
+		logger.info("로그인페이지 실행");
 		
-		if(memberService.getId(mVo)) {
-			return "redirect:/";
+		MemberVO user = memberService.getLogin(mVo);
+		if(user != null) {
+			model.addAttribute("user",user);
+			return "redirect:/nav";
 		}
 		return "redirect:/login";
+	}
+	@RequestMapping(value = "/nav", method = RequestMethod.GET)
+	public String navGet() {
+		logger.info("nav페이지 확인");
+		
+		return "nav";		
+	}
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logoutGet(HttpServletRequest request) {
+		logger.info("로그아웃 실행");		
+		HttpSession session = request.getSession();
+		session.removeAttribute("user");
+		return "redirect:/login";	
 	}
 }
